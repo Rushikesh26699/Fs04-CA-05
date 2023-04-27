7da576ac48ee6b1088c86dac456128be7be39819
RUSHIKESH P DHANAWADE <106026652+Rushikesh26699@users.noreply.github.com>
2023-04-27 17:41:13 +0530

    Home Landing Page

---
+++ script.js
@@ -0,0 +1,19 @@
+function Navbar(){
+    return <div className='maindiv'>
+            <nav className='navbar'>
+                <h1>LOGO</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+            <main className='mainDiv'>
+               <h1>Home</h1>
+               <p>Made with 💖 by <span>Rushikesh Dhanawade</span></p>
+            </main>
+    </div>
+}
+
+ReactDOM.render(<Navbar />,document.getElementById('root'))
